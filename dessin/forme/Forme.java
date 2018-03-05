11e2bc37478717ffe26825e74c8fa4fad369cc9b
BOOOYAA <34964215+BOOOYAA@users.noreply.github.com>
2018-03-05 22:33:11 +0100

    modif div
    suppresion des commentaire.

--- dessin/forme/Forme.java
+++ dessin/forme/Forme.java
@@ -8,20 +8,20 @@ class Forme extends JComponent {
 		Color bad = new Color(100,40,255);
 		Color purple = new Color(255,0,255);
 		Font corps = new Font("arial",Font.BOLD,24);//crée un font arial en gras de 24 pixel
-		ImageIcon icon = new ImageIcon("./login.jpg");//charge l'image dans l'icone afin de l'afficher
+		ImageIcon icon = new ImageIcon("./cercles.jpg");//charge l'image dans l'icone afin de l'afficher
 		Image img = icon.getImage();// crée un objet  image 
 		Graphics sndpinceau = pinceau.create();
 		if(this.isOpaque()){
 			sndpinceau.setColor(bad);
       		sndpinceau.fillRect(0, 0, this.getWidth(), this.getHeight());
 		}
-	/*sndpinceau.setColor(bad);
+	sndpinceau.setColor(bad);
     sndpinceau.drawRect(50,50,250,200);
     sndpinceau.setColor(Color.GREEN);
     sndpinceau.drawArc(60,60,25,25,0,360);
     sndpinceau.setColor(purple);
     sndpinceau.setFont(corps);
-    sndpinceau.drawString(">0<",64,76);*/
+    sndpinceau.drawString(">0<",64,76);
     sndpinceau.drawImage(img,0,0,null);
 
 	}
